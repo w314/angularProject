0167f452a7bd0dbbb9f2ca740244284f3952213c
Piroska Werner <piroska.werner@gmail.com>
2022-07-13 17:56:36 -0500

    fix: Remove references for deleted Recipe component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { RecipeComponent } from './recipe/recipe.component';
 import { ZooComponent } from './zoo/zoo.component';
 import { ZooAnimalComponent } from './zoo-animal/zoo-animal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RecipeComponent,
     ZooComponent,
     ZooAnimalComponent
   ],
